808bb81a70d90c40f5391600f773e0c52e07ed80
Hyungwon Lee <hueleev@gmail.com>
2021-08-13 17:30:34 +0900

    hackerRank

--- src/algorithmTest/test.java
+++ src/algorithmTest/test.java
@@ -1,14 +1,46 @@
 package algorithmTest;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
 
 public class test {
-    private static int[] priorities = {1, 1, 9, 1, 1, 1};
-    private static int location = 0;
+    private static int[] scoville = {1,2,3,9,10,12};
+    private static int K = 7;
 
     public static void main(String[] args) {
+        List<Integer> lowerList = new ArrayList<>();
+        for (int i : scoville) {
+            if (i < 7) {
+                lowerList.add(i);
+            }
+        }
 
+        System.out.println(lowerList);
+
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s= scan.nextLine();
+        scan.close();
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+        int N= 18;
+        System.out.println(N%2);
+        if (N % 2 == 0) {
+            System.out.println(N);
+            if (N >=2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        } else {
+            System.out.println("Weird");
+        }
     }
 }
 
---
+++ src/hackerRank/challenges30/Day2_Operators.java
@@ -0,0 +1,45 @@
+package hackerRank.challenges30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.hackerrank.com/challenges/30-operators/problem
+ */
+public class Day2_Operators {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        // Write your code here
+        double tip = (double) tip_percent / (double) 100 * meal_cost;
+        double tax = (double) tax_percent / (double) 100 * meal_cost;
+
+        System.out.println(Math.round((meal_cost+tip+tax)));
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Day2_Operators.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
